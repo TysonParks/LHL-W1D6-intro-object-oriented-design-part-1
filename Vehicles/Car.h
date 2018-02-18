f65f07294d99cba652e4b096063003ac25cc7c41
TysonParks <typarks@gmail.com>
2018-02-17 18:05:49 -0800

    Created Car class; subclass of Vehicle

---
+++ Vehicles/Car.h
@@ -0,0 +1,13 @@
+//
+//  Car.h
+//  Vehicles
+//
+//  Created by Tyson Parks on 2/17/18.
+//  Copyright © 2018 Designated Nerd Software. All rights reserved.
+//
+
+#import "Vehicle.h"
+
+@interface Car : Vehicle
+
+@end
---
+++ Vehicles/Car.m
@@ -0,0 +1,13 @@
+//
+//  Car.m
+//  Vehicles
+//
+//  Created by Tyson Parks on 2/17/18.
+//  Copyright © 2018 Designated Nerd Software. All rights reserved.
+//
+
+#import "Car.h"
+
+@implementation Car
+
+@end
